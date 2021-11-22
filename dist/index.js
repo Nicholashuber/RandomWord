28480a34ca3a7ea9d9adc930a909b1e438aa92a4
Nicholas Huber <anongenie@gmail.com>
2021-11-21 21:03:41 -0700

    update build

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWorld = void 0;
 var helloWorld = function () {
-    return 'Howdy!';
+    return 'Hello there!';
 };
 exports.helloWorld = helloWorld;
 //# sourceMappingURL=index.js.map
